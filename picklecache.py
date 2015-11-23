005aa3af96e899c8d84db56eaa84e6a5d83bca23
conekilla69 <wrcfan@verizon.net>
2015-11-23 04:46:49 +0000

    had a hard time with this.

---
+++ picklecache.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+"""Picklecasche"""
+
+
+import os
+import pickle
+
+
+class PickleCache(object)
+
+    def __init__PickleCache(self, file_path, autosync)
+        """
+        Arguments:
+        file_path (str, optional): default value = datastore.pk.
+        autosync (bool, optional): defalut value = False.
+        """
+        
+        file_path = datastore.pk
+        autosync = False
+
+        def __init__file_path (datastore.pk, _data, autosync)
+            datastore.pk = file_path
+            self._data = {}
+            autosync =
+
+    def _setitem_()
+        key
+        value
+        key, value = self._data
+
+    def _self.data()
+        _self.data(len)
+
+    def _getitem_ (key):
+        for key ():
+            key = self._data()
+
+        else Raise error
+        retrun TypeError, KeyError
+
+    def _delitem_ (key)
+
+    def load:
+        os.path.exists('self._file_path', 'r' )
+        os.path.getsize ('self._file_path, 'r')
+            load = pickel.load(self._data)
+        self._data.close
+
+    
